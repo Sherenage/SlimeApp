98d27f617b7e0f9ba2afc11822899b9c9d6084f3
Sherena <63917571+Sherenage@users.noreply.github.com>
2020-12-30 23:31:29 +0100

    Create GameViewHolder.kt

---
+++ app/src/main/java/com/example/slimeapp/presentation/main/GameViewHolder.kt
@@ -0,0 +1,7 @@
+package com.example.slimeapp.presentation.main
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class GameViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+}
